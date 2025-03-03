8e29b10c7a20a7379ba26e61e7295927bf2cefb0
kmax17 <max@maxkaya.com>
2025-03-03 10:57:52 -0500

    refactor: Change runtime to echo (#10)

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
-	"net/http"
-	"os"
-	"strings"
-	"time"
 )
 
 var (
@@ -29,62 +26,15 @@ func init() {
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
 
-	r := engine()
+	e := echo.New()
 
-	r.Use(gin.Recovery())
+	e.RouteNotFound("/*", ServeFile)
+	e.POST("/upload", UploadFile)
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
 
 	log.Info().Str("version", version).Str("commit", commit).Str("date", date)
 
-	if err := engine().Run(fmt.Sprintf(":%d", Config.Port)); err != nil {
-		log.Err(err).Msg("Error starting server")
-	}
-}
-
-func engine() *gin.Engine {
-	r := gin.New()
-
-	r.Use(func(c *gin.Context) {
-		if !strings.Contains(Config.Domain, c.Request.Host) {
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid host header"})
-			return
-		}
-		c.Header("X-Frame-Options", "DENY")
-		c.Header("Content-Security-Policy", "default-src 'self'; connect-src *; font-src *; script-src-elem * 'unsafe-inline'; img-src * data:; style-src * 'unsafe-inline';")
-		c.Header("X-XSS-Protection", "1; mode=block")
-		c.Header("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
-		c.Header("Referrer-Policy", "strict-origin")
-		c.Header("X-Content-Type-Options", "nosniff")
-		c.Header("Permissions-Policy", "geolocation=(),midi=(),sync-xhr=(),microphone=(),camera=(),magnetometer=(),gyroscope=(),fullscreen=(self),payment=()")
-		c.Next()
-	})
-
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-
-	r.Use(func(c *gin.Context) {
-		start := time.Now()
-		c.Next()
-		log.Info().
-			Int("status", c.Writer.Status()).
-			Dur("latency", time.Since(start)).
-			Str("client_ip", c.ClientIP()).
-			Str("method", c.Request.Method).
-			Str("path", c.Request.URL.Path).
-			Str("user_agent", c.Request.UserAgent()).
-			Msg("")
-	})
-
-	gin.DebugPrintRouteFunc = func(httpMethod, absolutePath, handlerName string, nuHandlers int) {
-		log.Debug().
-			Str("method", httpMethod).
-			Str("path", absolutePath).
-			Str("handler", handlerName).
-			Int("nuHandlers", nuHandlers).
-			Msg("")
-	}
-
-	r.NoRoute(ServeFile)
-
-	r.POST("/upload", UploadFile)
-
-	return r
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", Config.Port)))
 }
