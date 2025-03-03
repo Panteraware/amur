798c1b4b0f5c4447f25b4f3f10271b283a266e0f
kmax17 <max@maxkaya.com>
2025-03-03 14:30:52 -0500

    feat: Created resize image function

--- util.go
+++ util.go
@@ -14,7 +14,75 @@ import (
 	"strings"
 )
 
-func ResizeImage(filePath string) (string, error) {
+func ResizeImage(filePath string, width int, height int) (string, error) {
+	input, err := os.Open(filePath)
+	if err != nil {
+		log.Error().Err(err).Str("path", filePath).Str("util", "resize").Msg("failed to open file")
+		return "", err
+	}
+	defer input.Close()
+
+	fileExt := filepath.Ext(filePath)
+
+	output, err := os.Create(fmt.Sprintf("%s_thumb%s", strings.Replace(filePath, fileExt, "", -1), fileExt))
+	if err != nil {
+		log.Error().Err(err).Str("path", filePath).Str("util", "resize").Msg("failed to create file")
+		return "", err
+	}
+	defer output.Close()
+
+	var (
+		src image.Image
+	)
+
+	if fileExt == ".png" {
+		src, err = png.Decode(input)
+		if err != nil {
+			log.Error().Err(err).Str("path", filePath).Str("util", "resize").Msg("failed to decode png")
+			return "", err
+		}
+	} else if fileExt == ".jpg" || fileExt == ".jpeg" {
+		src, err = jpeg.Decode(input)
+		if err != nil {
+			log.Error().Err(err).Str("path", filePath).Str("util", "resize").Msg("failed to decode jpeg")
+			return "", err
+		}
+	} else if fileExt == ".webp" {
+		src, err = webp.Decode(input)
+		if err != nil {
+			log.Error().Err(err).Err(err).Str("path", filePath).Str("util", "resize").Msg("failed to decode webp")
+			return "", err
+		}
+	}
+
+	dst := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	draw.BiLinear.Scale(dst, dst.Rect, src, src.Bounds(), draw.Over, nil)
+
+	if fileExt == ".png" {
+		err := png.Encode(output, dst)
+		if err != nil {
+			log.Error().Err(err).Str("path", filePath).Str("util", "resize").Msg("failed to encode png")
+			return "", err
+		}
+	} else if fileExt == ".jpg" || fileExt == ".jpeg" {
+		err := jpeg.Encode(output, dst, nil)
+		if err != nil {
+			log.Error().Err(err).Str("path", filePath).Str("util", "resize").Msg("failed to encode jpeg")
+			return "", err
+		}
+	} else if fileExt == ".webp" {
+		err := webpbin.Encode(output, dst)
+		if err != nil {
+			log.Error().Err(err).Str("path", filePath).Str("util", "resize").Msg("failed to encode webp")
+			return "", err
+		}
+	}
+
+	return fmt.Sprintf("%s_thumb%s", strings.Replace(filePath, fileExt, "", -1), fileExt), nil
+}
+
+func ThumbnailImage(filePath string) (string, error) {
 	if strings.Contains(filePath, "_thumb") {
 		return "", nil
 	}
