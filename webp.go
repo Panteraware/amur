2d05f9e26f74dbe00dc0c0e08eb9bb779b118fda
maxpeterkaya <git@maxkaya.com>
2025-06-21 22:46:33 +0000

    feat: move encoder away from bin wrapper (#32)

--- webp.go
+++ webp.go
@@ -3,14 +3,10 @@ package main
 import (
 	"errors"
 	"github.com/rs/zerolog/log"
-	"image"
-	"image/jpeg"
-	"image/png"
 	"os"
+	"os/exec"
 	"path"
 	"strings"
-
-	"github.com/nickalie/go-webpbin"
 )
 
 func EncodeWebP(filePath string) (*string, error) {
@@ -24,13 +20,6 @@ func EncodeWebP(filePath string) (*string, error) {
 		return nil, errors.New("not an image")
 	}
 
-	file, err := os.Open(filePath)
-	if err != nil {
-		log.Error().Err(err).Str("file", filePath).Msg("failed to open file")
-
-		return nil, err
-	}
-
 	fileName := strings.Replace(filePath, ext, ".webp", -1)
 
 	output, err := os.Create(fileName)
@@ -41,27 +30,8 @@ func EncodeWebP(filePath string) (*string, error) {
 	}
 	defer output.Close()
 
-	var img image.Image
-
-	if ext == ".jpg" || ext == ".jpeg" {
-		img, err = jpeg.Decode(file)
-		if err != nil {
-			log.Error().Err(err).Str("file", filePath).Msg("failed to decode jpeg file")
-
-			return nil, err
-		}
-	}
-	if ext == ".png" {
-		img, err = png.Decode(file)
-		if err != nil {
-			log.Error().Err(err).Str("file", filePath).Msg("failed to decode png file")
-
-			return nil, err
-		}
-	}
-
-	if err := webpbin.Encode(output, img); err != nil {
-		output.Close()
+	err = exec.Command("cwebp", filePath, "-o", fileName).Run()
+	if err != nil {
 		log.Error().Err(err).Str("file", filePath).Msg("failed to encode webp file")
 	}
 
