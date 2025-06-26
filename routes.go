40c2239f1d935f90ac80a6f5f57778c96781d089
maxpeterkaya <git@maxkaya.com>
2025-06-26 00:06:38 +0000

    fix: remove optimization from serve (#37)

--- routes.go
+++ routes.go
@@ -6,11 +6,9 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/rs/zerolog/log"
 	"io"
-	"net/http"
 	"net/url"
 	"os"
 	"path"
-	"path/filepath"
 	"strings"
 )
 
@@ -28,78 +26,6 @@ func ServeFile(c echo.Context) error {
 
 	if !exists {
 		return c.NoContent(404)
-	}
-
-	mType := http.DetectContentType(ReadLimitedBytes(filePath, 512))
-
-	if strings.Contains(mType, "image") {
-		if Config.UseRedis {
-			if !strings.HasSuffix(filePath, ".webp") {
-				task, err := NewImageOptimizationTask(filePath)
-				if err != nil {
-					log.Error().Err(err).Str("path", filePath).Str("route", "serve").Str("task", TypeImageOptimization).Msg("error creating task")
-				}
-				info, err := AsynqClient.Enqueue(task)
-				if err != nil {
-					log.Error().Err(err).Str("path", filePath).Str("route", "serve").Str("enqueue", TypeImageOptimization).Msg("error enqueuing task")
-				}
-
-				log.Info().Str("id", info.ID).Str("task", TypeImageOptimization).Msg("queued task")
-			}
-
-			task, err := NewImageThumbnailTask(filePath)
-			if err != nil {
-				log.Error().Err(err).Str("path", filePath).Str("route", "serve").Str("task", TypeImageThumbnail).Msg("error creating task")
-			}
-			info, err := AsynqClient.Enqueue(task)
-			if err != nil {
-				log.Error().Err(err).Str("path", filePath).Str("route", "serve").Str("enqueue", TypeImageThumbnail).Msg("error enqueuing task")
-			}
-
-			log.Info().Str("id", info.ID).Str("task", TypeImageThumbnail).Msg("queued task")
-		} else {
-			if !strings.HasSuffix(filePath, ".webp") {
-				_, err := EncodeWebP(filePath)
-				if err != nil {
-					log.Error().Err(err).Str("path", filePath).Str("route", "serve").Msg("error encoding webp")
-					return err
-				}
-			}
-
-			_, err := ThumbnailImage(filePath)
-			if err != nil {
-				log.Error().Err(err).Str("path", filePath).Str("route", "serve").Msg("error thumbnailing image")
-				return err
-			}
-		}
-	} else if strings.Contains(mType, "video") {
-		if Config.CanConvertHLS {
-			go func() {
-				err := ConvertToHLS(filePath)
-				if err != nil {
-					log.Error().Err(err).Str("path", filePath).Str("route", "serve").Msg("error converting to hls")
-				}
-			}()
-		}
-
-		if Config.CanScaleVideo && len(Config.VideoScale) > 0 {
-			fullPath, _ := filepath.Abs(filePath)
-			//output, err := exec.Command(fmt.Sprintf("ffprobe -v error -select_streams v -show_entries stream=height -of csv=p=0:s=x \"%s\"", fullPath)).Output()
-			//if err != nil {
-			//	log.Error().Err(err).Str("path", filePath).Str("route", "serve").Msg("error executing ffprobe")
-			//}
-			//
-			//videoScale, err := strconv.Atoi(string(output))
-
-			for _, scale := range GetVideoScales(1080) {
-				go func() {
-					err := ScaleVideo(fullPath, scale)
-					if err != nil {
-						log.Error().Err(err).Str("path", filePath).Str("route", "serve").Msg("error scaling video")
-					}
-				}()
-			}
-		}
 	}
 
 	return c.File(filePath)
